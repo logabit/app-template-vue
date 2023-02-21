906685c9157eab8963b90e0a8abca118c4a6ffb4
Neel Dave <n.dave@logabit.com>
2023-02-21 18:43:25 +0530

    Change: make the app name configurable

--- hello-world/vue.config.js
+++ hello-world/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/api/v3/resource/io.pipeforce.hellovue/public/'
+    ? `/api/v3/resource/${process.env.APP_NAME}/public/`
     : '/'
 })
